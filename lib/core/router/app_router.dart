2caca16bb635cc2a9a4c3ae11fba322b60f1d03c
SmitBorad <smitborad0@gmail.com>
2025-09-18 08:42:20 -0700

    router changes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -14,29 +14,27 @@ class AppRouter {
         name: SplashPage.routeName,
         path: '/',
         builder: (context, state) => const SplashPage(),
+      ),
+      GoRoute(
+        name: LocationPermissionPage.routeName,
+        path: 'location-permission',
+        builder: (context, state) => const LocationPermissionPage(),
+      ),
+      GoRoute(
+        name: HomePage.routeName,
+        path: 'home',
+        builder: (context, state) => const HomePage(),
         routes: [
           GoRoute(
-            name: LocationPermissionPage.routeName,
-            path: 'location-permission',
-            builder: (context, state) => const LocationPermissionPage(),
+            name: LocationSearchPage.routeName,
+            path: 'location-search',
+            builder: (context, state) =>
+                LocationSearchPage(), // maybe passes args
           ),
           GoRoute(
-            name: HomePage.routeName,
-            path: 'home',
-            builder: (context, state) => const HomePage(),
-            routes: [
-              GoRoute(
-                name: LocationSearchPage.routeName,
-                path: 'location-search',
-                builder: (context, state) =>
-                    LocationSearchPage(), // maybe passes args
-              ),
-              GoRoute(
-                name: WeatherMapPage.routeName,
-                path: 'weather-map',
-                builder: (context, state) => WeatherMapPage(),
-              ),
-            ],
+            name: WeatherMapPage.routeName,
+            path: 'weather-map',
+            builder: (context, state) => WeatherMapPage(),
           ),
         ],
       ),
